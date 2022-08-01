89bd1540b3ff667fde7eb745c1ceb10abc5a3253
sleepy0owl <souravcristiano502@gmail.com>
2022-08-01 12:17:54 +0530

    Add get unicorn basket by user controller

--- controller/unicorn_basket.py
+++ controller/unicorn_basket.py
@@ -1,8 +1,9 @@
+import uuid
 from fastapi import APIRouter, Depends, HTTPException, status
 from models.models import UnicornBasket
 from database.database import SessionLocal
 from sqlalchemy.orm import session
-from database.models import UnicornBasketDB
+from database.models import UnicornBasketDB, UnicornsDB
 
 
 def get_db():
@@ -36,9 +37,17 @@ async def create_unicron_basket(unicorn_basket: UnicornBasket, db: session = Dep
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
 
-@router.get("/{user_uuid}", status_code=status.HTTP_200_OK)
-async def get_basket_by_user(user_uuid: str):
-    return {"uuid": user_uuid}
+@router.get("/{user_uuid}", status_code=status.HTTP_200_OK, response_model=UnicornBasket)
+async def get_basket_by_user(user_uuid: str, db: session = Depends(get_db)):
+    if user_uuid is not None:
+        response = db.query(UnicornBasketDB).filter(UnicornBasketDB.uuid == user_uuid).all()
+        unicorn_uuid = response[0].unicornUuid
+        unicorns = db.query(UnicornsDB).filter(UnicornsDB.uuid == unicorn_uuid).all()
+
+        response_value = UnicornBasket(uuid=user_uuid, unicorns=unicorns)
+        return response_value
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
 
 @router.delete("", status_code=status.HTTP_200_OK)
