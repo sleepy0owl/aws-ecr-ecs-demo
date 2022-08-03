9241862b67a51280714e758d2221478c59ba5fb9
sleepy0owl <souravcristiano502@gmail.com>
2022-08-03 23:28:35 +0530

    Use settings for env variables

--- database/database.py
+++ database/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://sourav:unishop123@192.168.0.124/unishop"
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}:{settings.MYSQL_PORT}/{settings.MYSQL_DATABASE}"
 
 engine = create_engine(url=SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from controller import users, health, unicorn_basket, unicorns
+from app.core.config import settings
 
 
 def get_application():
-    _app = FastAPI(title="MonotoMicro")
+    _app = FastAPI(title=settings.PROJECT_NAME)
 
     _app.add_middleware(
         CORSMiddleware,
